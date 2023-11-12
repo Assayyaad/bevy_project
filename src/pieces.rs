45d471e5af1e69de0f47458cb1806624bdf4c2e1
The Alpha <contact@nabilalsaiad.com>
2023-11-12 12:15:32 +0800

    style: change func and param names

--- src/pieces.rs
+++ src/pieces.rs
@@ -1,9 +1,9 @@
-use bevy::{prelude::*, transform};
-use bevy_vector_shapes::prelude::*;
+use bevy::prelude::*;
 
 use crate::{board::MAX, board::SIZE, input::Selection};
 
 const ORDER_LAYER: f32 = 5.0;
+const SCALER: f32 = 2.;
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum PieceColor {
@@ -35,7 +35,7 @@ impl Plugin for PiecesPlugin {
     fn build(&self, app: &mut App) {
         // app.add_state::<Game>()
         app.add_systems(Startup, spawn_pieces)
-            .add_systems(PostStartup, add_sprite)
+            .add_systems(PostStartup, load_sprites)
             .add_systems(Update, move_pieces);
     }
 }
@@ -68,9 +68,7 @@ fn spawn_pieces(mut commands: Commands) {
     }
 }
 
-const SCALER: f32 = 2.;
-
-fn add_sprite(
+fn load_sprites(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     query: Query<(Entity, &Piece)>,
@@ -100,12 +98,12 @@ fn add_sprite(
 fn move_pieces(
     mut commands: Commands,
     mut selection: ResMut<Selection>,
-    mut piece_query: Query<(Entity, &mut Piece, &mut Transform)>,
+    mut query: Query<(Entity, &mut Piece, &mut Transform)>,
 ) {
     if selection.new == Vec2::NEG_ONE {
         return;
     }
-    for (piece_id, mut piece, mut transform) in piece_query.iter_mut() {
+    for (id, mut piece, mut transform) in query.iter_mut() {
         let pos = Vec2::new(piece.x as f32, piece.y as f32);
         if pos == selection.old {
             piece.x = selection.new.x as u8;
@@ -114,7 +112,7 @@ fn move_pieces(
             transform.translation =
                 Vec3::new(selection.new.x * SIZE, selection.new.y * SIZE, ORDER_LAYER);
         } else if pos == selection.new {
-            commands.entity(piece_id).despawn();
+            commands.entity(id).despawn();
         }
     }
 
